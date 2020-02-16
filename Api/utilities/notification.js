53dd783d8d0efc4119d9f4196b302ee81267970e
Vinayak <sareenv@uni.coventry.ac.uk>
2020-02-16 02:43:34 +0000

    changes

--- Api/utilities/notification.js
+++ Api/utilities/notification.js
@@ -1,17 +1,23 @@
 'use strict'
 
-const twillo = require('twilio')
-const accountSid = 'ACf6e0e256a52580175c718059a888a1cb'
-const authToken = '2974a94f91a0b8f0d466cb65037f41ac';
-const client = twillo(accountSid, authToken)
+const twilio = require('twilio')
+const env = require('dotenv')
+env.config()
+
+const accountSid  = process.env.SID
+const authToken = process.env.Token
+const client = twilio(accountSid, authToken)
+const phoneNumber = process.env.Phone
 
 function sendMessage(message) {
     client.messages.create({
-     body: 'Your child is in wrong hands',
-     from: '+447480780255',
-     to: '+447510616477'
-    }).then(message => console.log(message.sid)) 
-    .catch(error => console.log(error.message))
+        body: 'CodePro',
+        from: phoneNumber,
+        to: '+447510616477'
+   }).then(message => console.log(message.sid)).catch((error) => {
+       console.log('error occured!', error)
+   })
 }
+
 
 module.exports = { sendMessage }
